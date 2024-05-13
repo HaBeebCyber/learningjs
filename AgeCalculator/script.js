d8999ee19f4d4a61587371663987c35de74f35ca
beeby01 <habeebjimoh29@gmail.com>
2024-05-13 10:22:19 +0100

    Oooop #DreamWithADeadline

--- AgeCalculator/script.js
+++ AgeCalculator/script.js
@@ -3,43 +3,43 @@ userInput.max = new Date().toISOString().split("T")[0];
 let result = document.getElementById("result");
 
 function calculateAge() {
-  let dob = new Date(userInput.value);
-
-  let d1 = dob.getDate();
-  let m1 = dob.getMonth() + 1;
-  let y1 = dob.getFullYear();
-
-  let today = new Date();
-
-  let d2 = today.getDate();
-  let m2 = today.getMonth() + 1;
-  let y2 = today.getFullYear();
-
-  let d3, m3, y3;
-  y3 = y2 - y1;
-
-  if (m2 >= m1) {
-    m3 = m2 - m1;
-  } else {
-    y3--;
-    m3 = 12 + m2 - m1;
-  }
-
-  if (d2 >= d1) {
-    d3 = d2 - d1;
-  } else {
-    m3--;
-    d3 = getDaysInMonth(y1, m1) + d2 - d1;
-  }
-
-  if (m3 < 0) {
-    m3 = 11;
-    y3--;
-  }
-  
-  if(userInput == " "){
-    result.innerHTML = `Please I don't have time to waste enter a valid date`; 
+  if(userInput.value == ""){
+    result.innerHTML = `<span>Please I don't have time to waste enter a valid date</span>`; 
   }else{
+    let dob = new Date(userInput.value);
+  
+    let d1 = dob.getDate();
+    let m1 = dob.getMonth() + 1;
+    let y1 = dob.getFullYear();
+  
+    let today = new Date();
+  
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
+  
+    let d3, m3, y3;
+    y3 = y2 - y1;
+  
+    if (m2 >= m1) {
+      m3 = m2 - m1;
+    } else {
+      y3--;
+      m3 = 12 + m2 - m1;
+    }
+  
+    if (d2 >= d1) {
+      d3 = d2 - d1;
+    } else {
+      m3--;
+      d3 = getDaysInMonth(y1, m1) + d2 - d1;
+    }
+  
+    if (m3 < 0) {
+      m3 = 11;
+      y3--;
+    }
+    
     result.innerHTML = `You are ${y3} years, ${m3} months and ${d3} days old`;
   }
 }
